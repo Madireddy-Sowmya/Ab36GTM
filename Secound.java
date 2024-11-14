32b878d9a2706faabb6ce0d4d7863ce5e34081b2
Sowmya <sowmyamadireddy92@gmail.com>
2024-11-13 21:48:27 -0600

    secound time

---
+++ Secound.java
@@ -0,0 +1,23 @@
+package Access;
+
+public class Secound {
+
+	private static void add()
+	{
+		System.out.println("adding");
+	}
+	public void sub()
+	{
+		System.out.println("subtracting");
+	}
+	protected void div()
+	{
+		System.out.println("dividing");
+	}
+	void mul()
+	{
+		System.out.println("multiplication");
+	}
+
+
+}
